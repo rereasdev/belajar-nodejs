ebad2475619cbfe974c51af08779757c810de41c
Rizqi Taufiq <rizqi.taufiq@iprospect.com>
2022-01-20 17:21:43 +0700

    first use expressJS

---
+++ 13-nodeJS-express-web-server/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hellow");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://locahost:${port}...`);
+});
+
+// const http = require("http");
+// const fs = require("fs");
+
+// const port = 3000;
+
+// const getPage = (path, result) => {
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       result.writeHead(404);
+//       result.write("Error: file not found");
+//     } else {
+//       result.write(data);
+//     }
+//     result.end();
+//   });
+// };
+// http
+//   .createServer((request, result) => {
+//     result.writeHead(200, {
+//       "Content-Type": "text/html",
+//     });
+//     const url = request.url;
+//     switch (url) {
+//       case "/about":
+//         getPage("./about.html", result);
+//         break;
+//       default:
+//         getPage("./index.html", result);
+//         break;
+//     }
+//   })
+//   .listen(port, () => {
+//     console.log(`Server is istening on port ${port}...`);
+//   });
